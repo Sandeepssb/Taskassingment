136b1cf2e2b2144bbbf097b401fb6806f551a6d4
Sandy <d.sandeepssb@gmail.com>
2020-12-14 15:58:07 +0530

    package

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import App from './components/App';
 import reducers from './reducers';
-import './css/main.css';
 
 
 ReactDOM.render(

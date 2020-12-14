b2b3f39f141c5a47f8ac9c020ef77db414c1ceca
Sandy <d.sandeepssb@gmail.com>
2020-12-14 16:13:24 +0530

    Updates

---
+++ precache-manifest.cb9a156778b80da635cec257da118d08.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dee9d9169f7e886db5719c9f3b4277c",
+    "url": "/Taskassingment/index.html"
+  },
+  {
+    "revision": "23fb6285d2a45e88cc65",
+    "url": "/Taskassingment/static/js/2.fe430403.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Taskassingment/static/js/2.fe430403.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48ad177608789d323a01",
+    "url": "/Taskassingment/static/js/main.30e07cf4.chunk.js"
+  },
+  {
+    "revision": "8dcfdd02da7c81a1a1d0",
+    "url": "/Taskassingment/static/js/runtime-main.4a88d267.js"
+  }
+]);
